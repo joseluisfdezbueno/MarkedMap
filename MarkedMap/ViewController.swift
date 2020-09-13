b131410e01c714927a853cec5b469e8eeea5b2c9
José Luis Fernández Bueno <joseluis.fernandez@intelygenz.com>
2020-09-13 10:01:19 +0200

    Initial Commit

---
+++ MarkedMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarkedMap
+//
+//  Created by Jose Luis Fernandez on 13/09/2020.
+//  Copyright © 2020 joseluisfdezbueno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
